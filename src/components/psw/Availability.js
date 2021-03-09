b66130ec634ebfce2123e78c8b99a796b8c3bb3e
Mila Ashtari <mashtari1991@gmail.com>
2021-03-09 13:46:21 -0500

    availibilty and bookings calender created

--- src/components/psw/Availability.js
+++ src/components/psw/Availability.js
@@ -1,14 +1,35 @@
 import React, { useState } from "react";
-import { Calendar, MuiPickersUtilsProvider } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
+import Paper from "@material-ui/core/Paper";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  DayView,
+  WeekView,
+  Appointments
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 const Availability = () => {
-    const [date, setDate]=useState(new Date())
-    
+  const currentDate = "2018-11-01";
+  const schedulerData = [
+    {
+      startDate: "2018-11-01T09:45",
+      endDate: "2018-11-01T11:00",
+      title: "Meeting",
+    },
+    {
+      startDate: "2018-11-01T12:00",
+      endDate: "2018-11-01T13:30",
+      title: "Go to a gym",
+    },
+  ];
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <Calendar date={date} onChange={(date)=> setDate(date)} allowKeyboardControl/>
-    </MuiPickersUtilsProvider>
+    <Paper>
+      <Scheduler data={schedulerData}>
+        <ViewState currentDate={currentDate} />
+        <WeekView startDayHour={9} endDayHour={24} />
+        <Appointments />
+      </Scheduler>
+    </Paper>
   );
 };
 
