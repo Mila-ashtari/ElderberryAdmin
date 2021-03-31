52df60308705f75767fae304d1ff8880ff3c137e
Mila Ashtari <mashtari1991@gmail.com>
2021-03-31 17:29:33 -0400

    client page updated

--- src/components/client/Client.js
+++ src/components/client/Client.js
@@ -2,29 +2,21 @@ import React, { Fragment, useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 import { getClient } from "../../actions/client";
-// import Profile from "./Profile";
-// import Documents from "./Documents";
-// // import Bookings from "./Bookings";
-// import Availability from "./Availability";
-// import User from "../user/User";
-// // import Schedule from "./Schedule"
-// import TestBookings from "./TestBookings";
+import Profile from "./Profile";
+import TestBookings from "./TestBookings";
+
 
 const Client = (props) => {
   const { client, getClient } = props;
-  const { user, skills, profile, schedule, currentBookings, id } =
+  const { currentBookings, id } =
     props.client !== undefined && props.client;
-//   const tabs = props.client !== undefined && [
-//     {
-//       label: "Profile",
-//       component: <Profile profile={profile} skills={skills} />,
-//     },
-//     { label: "Documentation", component: <Documents client={client} /> },
-//     // { label: "Schedule", component: <Schedule {...{schedule, currentBookings}}/> },
-//     { label: "Availability", component: <Availability schedule={schedule} /> },
-//     // { label: "Bookings", component: <Bookings /> },
-//     { label: "TestBookings", component: <TestBookings /> },
-//   ];
+  const tabs = props.client !== undefined && [
+    {
+      label: "Profile",
+      component: <Profile client={client} />,
+    },
+    { label: "TestBookings", component: <TestBookings /> },
+  ];
 
   useEffect(() => {
     getClient(props.match.params.id);
