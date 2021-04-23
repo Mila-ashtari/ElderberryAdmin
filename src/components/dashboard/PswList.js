c458171041f5ab8f1b9b1a55b742cc340aef2739
Mila Ashtari <mashtari1991@gmail.com>
2021-04-23 12:57:44 -0400

    search bar added to psw tab

--- src/components/dashboard/PswList.js
+++ src/components/dashboard/PswList.js
@@ -1,18 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { TextField, InputAdornment } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 import { getPsws } from "../../actions/psw";
 import UserList from "../user/UserList";
 
 function PswList(props) {
-  const { getPsws, psws } = props;
+  const { getPsws, pswList } = props;
+  const [searchId, setSearchId] = useState("");
   useEffect(() => {
     getPsws();
   }, [getPsws]);
-  return <UserList users={psws} type="psw" />;
+
+  const dynamicpswList = () => {
+    return pswList.filter((psw) => {
+      psw.id.includes(searchId);
+    });
+  };
+  return (
+    <>
+      <TextField
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      ></TextField>
+      <UserList users={pswList} type="psw" />
+    </>
+  );
 }
 
 const mapStateToProps = (state) => {
-  return { psws: Object.values(state.psws) };
+  return { pswList: Object.values(state.psws) };
 };
 export default connect(mapStateToProps, { getPsws })(PswList);
